82538d8fef8629f58ac60f519bef61885077e067
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-09-28 02:46:02 +0700

    Create install_go.sh

---
+++ script/install_go.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Обновление системы
+sudo pacman -Syu
+echo "arch update"
+sudo pacman-S go
+echo "go install"
+go version
+mkdir -p ~/go/{bin,pkg,src}
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
